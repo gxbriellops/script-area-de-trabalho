cf50d8c0a9529fe2e096059ab5a671db27950377
Gabriel Lopes <gabriellopes.ct07@gmail.com>
2025-01-13 10:26:04 -0300

    fix

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pyautogui
 import time
 
-pyautogui.PAUSE = 1
+pyautogui.PAUSE = 0.5
 
 # verificar se o mouse esta na area de trabalho principal
 pyautogui.hotkey('win', 'ctrl', 'right')
@@ -14,13 +14,14 @@ pyautogui.click(x=958, y=1060)
 pyautogui.hotkey('win', 'up')
 
 # clica na agenda e arrasta para a direita
-pyautogui.click(x=332, y=31, duration=0.5)
+time.sleep(0.5)	
+pyautogui.click(x=332, y=31)
 pyautogui.dragTo(x=1920, y=500, duration=0.5)
 
 # clica na segunda pagina
 pyautogui.click(x=419, y=548, duration=0.5)
 pyautogui.scroll(-1000)	
-time.sleep(1)
+time.sleep(0.5)
 pyautogui.click(x=1512, y=138)
 pyautogui.click(x=1519, y=181)
 pyautogui.click(x=1568, y=136)
@@ -33,7 +34,7 @@ time.sleep(0.5)
 pyautogui.press('win')
 pyautogui.write('WhatsApp')
 pyautogui.press('enter')
-time.sleep(1)
+time.sleep(0.5)
 
 # comando para voltar a area de trabalho principal
 pyautogui.hotkey('win', 'ctrl', 'right')
